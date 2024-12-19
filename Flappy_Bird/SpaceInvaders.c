ac7f27504d2548431cbadc8aefc92ca10472692e
Hassan Azmy <hassanhussien13579@gmail.com>
2024-12-19 22:28:03 +0200

    Project's initial commit

---
+++ Flappy_Bird/SpaceInvaders.c
@@ -0,0 +1,61 @@
+#include "..//tm4c123gh6pm.h"
+#include "Nokia5110.h"
+#include "Random.h"
+#include "TExaS.h"
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void Timer2_Init(unsigned long period);
+void Delay100ms(unsigned long count); // time delay in 0.1 seconds
+unsigned long TimerCount;
+unsigned long Semaphore;
+
+
+// *************************** Images ***************************
+
+
+
+// *************************** Capture image dimensions out of BMP**********
+#define BUNKERW     ((unsigned char)Bunker0[18])
+#define ENEMY30W    ((unsigned char)SmallEnemy30PointA[18])
+#define LASERW      ((unsigned char)Laser0[18])
+#define MISSILEH    ((unsigned char)Missile0[22])
+#define PLAYERW     ((unsigned char)PlayerShip0[18])
+
+int main(void){
+  TExaS_Init(SSI0_Real_Nokia5110_Scope);  // set system clock to 80 MHz
+  Random_Init(1);
+  Nokia5110_Init();
+  Nokia5110_ClearBuffer();
+	Nokia5110_DisplayBuffer();      // draw buffer
+  
+  while(1){
+  }
+
+}
+
+
+// You can use this timer only if you learn how it works
+void Timer2_Init(unsigned long period){ 
+  unsigned long volatile delay;
+  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+  delay = SYSCTL_RCGCTIMER_R;
+  TimerCount = 0;
+  Semaphore = 0;
+  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAILR_R = period-1;    // 4) reload value
+  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
+  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC	// interrupts enabled in the main program after all devices initialized	// vector number 39, interrupt number 23
+  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+}
+void Timer2A_Handler(void){ 
+  TIMER2_ICR_R = 0x00000001;   // acknowledge timer2A timeout
+  TimerCount++;
+  Semaphore = 1; // trigger
+}
+
